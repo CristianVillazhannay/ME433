b948abff70d4e6c568ecc152303203a271ca6260
CristianVillazhannay <cristianvillazhannay2023@u.northwestern.edu>
2022-04-06 21:07:11 -0500

    HW3

---
+++ HW3.X/PIC32.h
@@ -0,0 +1,14 @@
+//Here is the header file for the PIC32.h code. 
+#ifndef PIC32__H__
+#define PIC32__H__
+
+#include<xc.h>           // processor SFR definitions
+#include<sys/attribs.h>  // __ISR macro
+
+#define PIC32_SYS_FREQ 48000000ul    // 48 million Hz
+
+void PIC32_Startup(void);
+void PIC32_ReadUART1(char * string, int maxLength);
+void PIC32_WriteUART1(const char * string );
+
+#endif
